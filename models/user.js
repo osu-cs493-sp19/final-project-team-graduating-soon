5890b3db5c6e4feb1273fa8311cf65344280daa5
Justin Sherburne <sherburj@oregonstate.edu>
2019-06-11 15:33:15 -0700

    New Stuff and Things for logins

--- models/user.js
+++ models/user.js
@@ -4,89 +4,91 @@
 
 const mongo = require("../lib/mongo");
 const { extractValidFields } = require("../lib/validation");
-
+const { getDBReference } = require('../lib/mongo');
 const bcrypt = require("bcryptjs");
 const { generateAuthToken, requireAuthentication } = require('../lib/auth');
+const { ObjectId } = require('mongodb');
 
 const mysqlPool = require('../lib/mysqlPool');
 /*
  * Schema describing required/optional fields of a user object.
  */
 const UserSchema = {
-  userid: { required: false },
-  name: { required: true },
-  email: { required: true },
-  password: { required: true },
-  role: { required: true }
-};
+    id: { required: false },
+    name: { required: true },
+    email: { required: true },
+    password: { required: true },
+    role: { required: true },
+  };
 exports.UserSchema = UserSchema;
 
 /*
  * Executes a MySQL query to insert a new user into the database.  Returns
  * a Promise that resolves to the ID of the newly-created user entry.
  */
-function insertNewUser(user) {
-  return bcrypt.hash(user.password, 8)
-    .then((passwordHash) => {
-      return new Promise((resolve, reject) => {
-        const userValues = {
-          name: user.name,
-          password: passwordHash,
-          email: user.email,
-		  role: user.role,
-        };
-        mysqlPool.query(
-          'INSERT INTO users SET ?',
-          userValues,
-          function (err, result) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(result.insertId);
-            }
-          }
-        );
-      });
-    });
-}
-exports.insertNewUser = insertNewUser;
+exports.insertNewUser = async function (user) {
+    const userToInsert = extractValidFields(user, UserSchema);
+    const db = getDBReference();
+    const collection = db.collection('users');
+  
+    const passwordHash = await bcrypt.hash(userToInsert.password, 8);
+    userToInsert.password = passwordHash;
+  
+    const result = await collection.insertOne(userToInsert);
+    return result.insertedId;
+};
 
+/*
+* Fetch a user from the DB based on user ID.
+*/
+async function getUserById(id, includePassword) {
+    const db = getDBReference();
+    const collection = db.collection('users');
+	const projection = includePassword ? {} : { password: 0 };
+	const results = await collection
+		.find({ _id: new ObjectId(id) })
+		.project(projection)
+		.toArray();
+	return results[0];
+};
+exports.getUserById = getUserById;
 
-function getUserByEmail(userEmail) {
-  return new Promise((resolve, reject) => {
-    mysqlPool.query(
-	'SELECT * FROM users WHERE email = ?',
-	 [ userEmail ], 
-	function (err, results) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results[0]);
-      }
-    });
-  });
-}
+/*
+* Fetch a user from the DB based on user email.
+*/
+async function getUserByEmail(userEmail, includePassword) {
+    const db = getDBReference();
+    const collection = db.collection('users');
+    const projection = includePassword ? {} : { password: 0 };
+    const results = await collection
+        .find({ email: userEmail })
+        .project(projection)
+        .toArray();
+    return results[0];
+};
 exports.getUserByEmail = getUserByEmail;
 
 
-
-/*
- * Fetch a user from the DB based on user ID.
- */
-function getUserByID(userID) {
-  return new Promise((resolve, reject) => {
-    mysqlPool.query('SELECT * FROM users WHERE id = ?', [ userID ], function (err, results) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results[0]);
+async function getCoursesByInstructorId(id) {
+    const db = getDBReference();
+    const collection = db.collection('users');
+    const results = await collection.aggregate([
+      {
+        $match: { _id: id }
+      },
+      {
+        $lookup: {
+          from: "courses",
+          localField: "_id",
+          foreignField: "instructorId",
+          as: "instructorCourses"
+        }
       }
-    });
-  });
-}
-exports.getUserByID = getUserByID;
-
-
+    ]).toArray();
+    console.log("instructor Courses results: ", results[0]);
+    return results[0];
+};
+exports.getCoursesByInstructorId = getCoursesByInstructorId;
 
 
 function deleteUserByID(id) {
@@ -131,15 +133,8 @@ function updateUserByID(id, user) {
 }
 
 
-
-/*
-
-exports.validateUser = async function(id, password) {
-  const user = await getUserById(id, true);
-  console.log("password: ", password);
-  console.log("user.password: ", await user.password, user.name);
-  console.log("bcrypt.compare: ", await bcrypt.compare(password, user.password));
-  const authenticated = user && (await bcrypt.compare(password, user.password));
-  return authenticated;
+ exports.validateUser = async function (email, password) {
+    const user = await getUserByEmail(email, true);
+    const authenticated = user && await bcrypt.compare(password, user.password);
+    return authenticated;
 };
-*/
